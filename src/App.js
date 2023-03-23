98664563bc92ee27394787cd3f887f4e589f2042
Kalok Cheung <kalok.cheung@home-plus.hk>
2023-03-23 11:37:55 +0800

    Feat: Updated CSS and added custom font, updated components to accept mock data

--- src/App.js
+++ src/App.js
@@ -12,12 +12,14 @@ function BusEtaTime(props) {
   );
 }
 
-function BusEtaTimeList() {
+function BusEtaTimeList(props) {
   return (
       <span className="Eta-time-list">
-        <BusEtaTime minute="2" real="true"/>
-        <BusEtaTime minute="6"/>
-        <BusEtaTime minute="10"/>
+        {
+            props.eta.map(function (item) {
+                return <BusEtaTime minute={item.time}/>
+            })
+        }
       </span>
   );
 }
@@ -28,29 +30,161 @@ function BusEtaRow(props) {
         <span className="route">{props.route}</span>
         <span className="stop">{props.stop}</span>
         <span className="dest">TO: {props.dest}</span>
-        <BusEtaTimeList/>
+        <BusEtaTimeList eta={props.eta}/>
       </div>
   );
 }
 
 function App() {
-  let data = [
-      {route: "278X", destEn: "TSUEN WAN (NINA TOWER)", stopNameEn: "KIN CHUEN STREET KWAI CHUNG"},
-      {route: "235", destEn: "TSUEN WAN (CIRCULAR)", stopNameEn: "KIN CHUEN STREET KWAI CHUNG"},
-      {route: "48X", destEn: "TSUEN WAN (BAYVIEW GARDEN)", stopNameEn: "KIN CHUEN STREET KWAI CHUNG"},
-      {route: "290X", destEn: "TSUEN WAN WEST STATION", stopNameEn: "KIN CHUEN STREET KWAI CHUNG"},
-      {route: "290", destEn: "TSUEN WAN WEST STATION", stopNameEn: "KIN CHUEN STREET KWAI CHUNG"},
-      {route: "73X", destEn: "TSUEN WAN (NINA TOWER)", stopNameEn: "KIN CHUEN STREET KWAI CHUNG"}
+    let data = [
+      {
+          route: "278X",
+          destEn: "TSUEN WAN (NINA TOWER)",
+          stopNameEn: "KIN CHUEN STREET KWAI CHUNG",
+          etaDetails: [
+              {
+                  time: 2,
+                  remarkEn: "",
+                  remarkTc: ""
+              },
+              {
+                  time: 8,
+                  remarkEn: "",
+                  remarkTc: ""
+              },
+              {
+                  time: 20,
+                  remarkEn: "",
+                  remarkTc: ""
+              }
+          ]
+      },
+      {
+          route: "235",
+          destEn: "TSUEN WAN (CIRCULAR)",
+          stopNameEn: "KIN CHUEN STREET KWAI CHUNG",
+          etaDetails: [
+              {
+                  time: 2,
+                  remarkEn: "",
+                  remarkTc: ""
+              },
+              {
+                  time: 8,
+                  remarkEn: "",
+                  remarkTc: ""
+              },
+              {
+                  time: 20,
+                  remarkEn: "",
+                  remarkTc: ""
+              }
+          ]
+      },
+      {
+          route: "48X",
+          destEn: "TSUEN WAN (BAYVIEW GARDEN)",
+          stopNameEn: "KIN CHUEN STREET KWAI CHUNG",
+          etaDetails: [
+              {
+                  time: 2,
+                  remarkEn: "",
+                  remarkTc: ""
+              },
+              {
+                  time: 8,
+                  remarkEn: "",
+                  remarkTc: ""
+              },
+              {
+                  time: 20,
+                  remarkEn: "",
+                  remarkTc: ""
+              }
+          ]
+      },
+      {
+          route: "290X",
+          destEn: "TSUEN WAN WEST STATION",
+          stopNameEn: "KIN CHUEN STREET KWAI CHUNG",
+          etaDetails: [
+              {
+                  time: 2,
+                  remarkEn: "",
+                  remarkTc: ""
+              },
+              {
+                  time: 8,
+                  remarkEn: "",
+                  remarkTc: ""
+              },
+              {
+                  time: 20,
+                  remarkEn: "",
+                  remarkTc: ""
+              }
+          ]
+      },
+      {
+          route: "290",
+          destEn: "TSUEN WAN WEST STATION",
+          stopNameEn: "KIN CHUEN STREET KWAI CHUNG",
+          etaDetails: [
+              {
+                  time: 2,
+                  remarkEn: "",
+                  remarkTc: ""
+              },
+              {
+                  time: 8,
+                  remarkEn: "",
+                  remarkTc: ""
+              },
+              {
+                  time: 20,
+                  remarkEn: "",
+                  remarkTc: ""
+              }
+          ]
+      },
+      {
+          route: "73X",
+          destEn: "TSUEN WAN (NINA TOWER)",
+          stopNameEn: "KIN CHUEN STREET KWAI CHUNG",
+          etaDetails: [
+              {
+                  time: 2,
+                  remarkEn: "",
+                  remarkTc: ""
+              },
+              {
+                  time: 8,
+                  remarkEn: "",
+                  remarkTc: ""
+              },
+              {
+                  time: 20,
+                  remarkEn: "",
+                  remarkTc: ""
+              }
+          ]
+      }
   ];
+
+    let result;
+
+    if (data.length > 0) {
+        result = data.map(function(item, i){
+            return <BusEtaRow route={item.route} stop={item.stopNameEn} dest={item.destEn} eta={item.etaDetails}/>
+        })
+    } else {
+        result = <div className="ErrorBox">No ETA available</div>;
+    }
 
   return (
     <div className="App">
       <div className="Header">Bus Found</div>
-        {
-          data.map(function(item, i){
-            return <BusEtaRow route={item.route} stop={item.stopNameEn} dest={item.destEn}/>
-          })
-        }
+        {result}
     </div>
   );
 }
